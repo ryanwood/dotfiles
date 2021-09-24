83cf1dd5175db98beb56b1c067b7ecfabc2c0ee0
Ryan Wood <ryan@moonclerk.com>
2021-09-24 15:29:44 -0400

    add exports for postgresql@10 package
    - needed to compile pg gem

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -4,6 +4,13 @@ if status --is-interactive
   set -p PATH .git/safe/../../bin
 end
 
+# For brew installed postgresql@10
+fish_add_path /usr/local/opt/postgresql@10/bin
+set -gx LDFLAGS "-L/usr/local/opt/postgresql@10/lib"
+set -gx CPPFLAGS "-I/usr/local/opt/postgresql@10/include"
+set -gx PKG_CONFIG_PATH "/usr/local/opt/postgresql@10/lib/pkgconfig"
+
+# General exports
 set -gx EDITOR nvim
 set -gx RIPGREP_CONFIG_PATH ~/.ripgreprc
 set -gx FZF_DEFAULT_COMMAND "rg --files"
