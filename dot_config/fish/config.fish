d16fd5e441a4c4384015c69c255ebe35611b9e9a
Ryan Wood <ryan@moonclerk.com>
2024-03-21 20:07:36 -0400

    use nvim (not hx) as $EDITOR

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,7 +1,7 @@
 if status --is-interactive
-  source /opt/homebrew/opt/asdf/libexec/asdf.fish
-  # Add bin path after rbenv
-  set -p PATH .git/safe/../../bin
+    source /opt/homebrew/opt/asdf/libexec/asdf.fish
+    # Add bin path after rbenv
+    set -p PATH .git/safe/../../bin
 end
 
 fish_add_path ~/.bin /opt/homebrew/bin /Applications/Postgres.app/Contents/Versions/latest/bin
@@ -14,9 +14,10 @@ set hydro_color_prompt --dim $fish_color_command
 set hydro_color_duration --dim $fish_color_command
 
 # General exports
-set -gx EDITOR hx
+set -gx EDITOR nvim
 set -gx RIPGREP_CONFIG_PATH ~/.ripgreprc
 set -gx FZF_DEFAULT_COMMAND "rg --files"
-set -gx FZF_DEFAULT_OPTS "-m"
+set -gx FZF_DEFAULT_OPTS -m
 
 source ~/.config/fish/abbreviations.fish
+
