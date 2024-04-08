b23c3d4dfc552a66da3e289f47d343242d2d3970
Ryan Wood <ryan@moonclerk.com>
2024-04-08 16:17:30 -0400

    fix cmd-s to save, remove swap line

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -2,13 +2,14 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+vim.keymap.set("n", "<D-s>", ":w<cr>", { desc = "Save file" })
+vim.keymap.set("i", "<D-s>", "<esc>:w<cr>i", { desc = "Save file" })
+vim.keymap.set("n", "<leader>fw", ":w<cr>", { desc = "Save file" })
+
 vim.keymap.set("n", "<leader>z", "zMzvzz", { desc = "Refocus folds" })
-vim.keymap.set("n", "<leader>fw", ":w<cr>", { desc = "Write file" })
 
 vim.keymap.set("n", "[<space>", "O<Esc>j", { desc = "Add line above" })
 vim.keymap.set("n", "]<space>", "o<Esc>k", { desc = "Add line below" })
-vim.keymap.set("n", "[l", "ddkP", { desc = "Swap with line above" })
-vim.keymap.set("n", "]l", "ddp", { desc = "Swap with line below" })
 
 vim.keymap.set("n", "<leader>rc", ":Econtroller<cr>", { desc = "Rails: Go to controller" })
 vim.keymap.set("n", "<leader>rv", ":Eview<cr>", { desc = "Rails: Go to view" })
