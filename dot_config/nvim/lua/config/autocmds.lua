40234d2c92c7d0a236195e9ef8d9be5ee763f348
Ryan Wood <ryan@moonclerk.com>
2024-06-06 11:20:07 -0400

    strip trailing whitespace

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -2,3 +2,4 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, { pattern = "*.thor", command = "set filetype=ruby" })
+vim.api.nvim_create_autocmd({ "BufWritePre" }, { pattern = { "*" }, command = [[%s/\s\+$//e]] })
