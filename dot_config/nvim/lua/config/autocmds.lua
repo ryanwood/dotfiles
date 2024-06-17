6099da27cf5bcfdc29dbfe8c130a0a5ca3c1ce98
Ryan Wood <ryan@moonclerk.com>
2024-06-17 12:06:07 -0400

    syntax for .jb files

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -1,5 +1,5 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
-vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, { pattern = "*.thor", command = "set filetype=ruby" })
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, { pattern = "*.thor, *.jb", command = "set filetype=ruby" })
 vim.api.nvim_create_autocmd({ "BufWritePre" }, { pattern = { "*" }, command = [[%s/\s\+$//e]] })
