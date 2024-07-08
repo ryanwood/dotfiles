719d977a081befaf43f3169d81579ddca29a0120
Ryan Wood <ryan@moonclerk.com>
2024-07-08 10:55:44 -0400

    fix fish/asdf/tmux debacle
    I upgraded asdf which broke in tmux. Upgraded tmux, asdf, and fish.
    Still broken. PATH is built differently in a subshell. Since the asdf
    script adds to PATH directly vs fish_add_path, the PATH was reorder when
    launching a subshell (or tmux) so none of the asdf versions were used.
    
    This explicit pathing should work now.

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,11 +1,3 @@
-if status --is-interactive
-    source /opt/homebrew/opt/asdf/libexec/asdf.fish
-    # Add bin path after rbenv
-    set -p PATH .git/safe/../../bin
-end
-
-fish_add_path ~/.bin /opt/homebrew/bin /Applications/Postgres.app/Contents/Versions/latest/bin
-
 source ~/.config/fish/themes/tokyonight_night.fish
 
 # Hydro prompt settings
@@ -22,3 +14,13 @@ set -gx FZF_DEFAULT_COMMAND "rg --files"
 set -gx FZF_DEFAULT_OPTS -m
 
 source ~/.config/fish/abbreviations.fish
+
+# Using set PATH directly as that's what ASDF does.
+# fish_set_path reorders items in subshells and breaks TMUX.
+if not contains /Users/ryanwood/bin $PATH
+    set -gx --prepend PATH /Users/ryanwood/bin /opt/homebrew/bin /Applications/Postgres.app/Contents/Versions/latest/bin
+end
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
+if not contains .git/safe/../../bin $PATH
+    set -gx --prepend PATH .git/safe/../../bin
+end
