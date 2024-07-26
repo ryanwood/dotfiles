2e5c1e67404c770fad4121b6e2f4a505e5271cda
Ryan Wood <ryan@moonclerk.com>
2024-07-26 14:12:08 -0400

    fix ruby-lsp and standardrb to run with local bundle version

--- dot_config/nvim/lua/plugins/core.lua
+++ dot_config/nvim/lua/plugins/core.lua
@@ -98,6 +98,21 @@ return {
   { "slim-template/vim-slim" },
   { "mpas/marp-nvim" },
   {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        -- https://github.com/ianchesal/dotfiles/blob/main/nvim/lua/plugins/nvim-lspconfig.lua#L32
+        ruby_lsp = {
+          cmd = { "bundle", "exec", "ruby-lsp" },
+          -- init_options = { formatter = "auto", },
+        },
+        standardrb = {
+          cmd = { "bundle", "exec", "standardrb" },
+        },
+      },
+    },
+  },
+  {
     "stevearc/conform.nvim",
     opts = {
       formatters = {
